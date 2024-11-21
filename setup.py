e18690981c56e4a472fa57b2bcc9fcd235d7e5d7
Paul Kreymborg <frenebo@gmail.com>
2024-11-21 15:20:33 -0500

    forgot to update setup py

--- setup.py
+++ setup.py
@@ -6,7 +6,17 @@ setup(
     description="Vertex Model Tutorial",
     author="Rastko Sknepnek",
     license="MIT",
-    packages=['VMToolkit', 'VMToolkit.VM', 'VMToolkit.VMAnalysis', 'VMToolkit.VMAnalysis.Graner', 'VMToolkit.VMAnalysis.utils', 'VMToolkit.VMAnalysis.utils.HalfEdge'],
+    packages=[
+        'VMToolkit',
+        'VMToolkit.VM',
+        'VMToolkit.config_builder',
+        'VMToolkit.config_builder.open',
+        'VMToolkit.config_builder.periodic',
+        'VMToolkit.VMAnalysis',
+        'VMToolkit.VMAnalysis.Graner',
+        'VMToolkit.VMAnalysis.utils',
+        'VMToolkit.VMAnalysis.utils.HalfEdge',
+        ],
     cmake_install_dir='VMToolkit/VM',
     python_requires=">=3.8",
 )
