1847c8971b76cc9e83105ae3379dda92a70a9d6b
Paul Kreymborg <frenebo@gmail.com>
2024-12-05 09:38:12 -0500

    running a sim

--- 2024_tests/run_hexagonal.py
+++ 2024_tests/run_hexagonal.py
@@ -3,18 +3,21 @@ from hexagonal_test.analytic_tools.find_hexagon_rest_area import HexagonalModel,
 
 from VMToolkit.config_builder.open.make_honeycomb import create_honeycomb_json
 from VMToolkit.config_builder.open.honeycomb_lattice import HoneycombLattice
-
+from VMToolkit.VM import Tissue, System, Force, Integrate, Topology, Dump, Simulation
 
 if __name__ == "__main__":
     hex_model = HexagonalModel()
     
     A0_model = 20.0
     P0_model = 1.0
+    kappa = 1.0     # area stiffness
+    gamma = 1.0     # perimeter stiffness
+
     res = hex_model.find_rest_size_of_hexagon(
         A_0=A0_model,
         P_0=P0_model,
-        K=1,
-        Gamma=1,
+        K=kappa,
+        Gamma=gamma,
     )
     rest_side_length = res["rest_side_length"]
     print(res)
@@ -29,8 +32,71 @@ if __name__ == "__main__":
     h.set_energy_params(A0=A0_model,P0=P0_model)    
     h.json_out("scratch/example.json")
     
+    ##### Running sim
+    tissue  = Tissue()                                               # initialise mesh
+    sim_sys = System(tissue)                                         # base object for the system
+    forces = Force(sim_sys)                                          # handles all types of forces
+    integrators = Integrate(sim_sys, forces, args.seed)              # handles all integrators
+    topology = Topology(sim_sys, forces)                             # handles all topology changes (T1, division, ingression)
+    dumps = Dump(sim_sys, forces)                                    # handles all data output 
+    simulation = Simulation(sim_sys, integrators, forces, topology)  # simulation object
+
+    # #################################################################
+    #
+    # Create the initial configuration and read it
+    #
+    # #################################################################
+
+    sim_sys.read_input("scratch/example.json")           # read input configuration
+
+
+    # #################################################################
+    #
+    # Add forces to the system
+    #
+    # #################################################################
+
+        
+
+    forces.add('area')         # add area force form term E = 0.5*kappa*(A-A0)^2
+    forces.add('perimeter')    # add perimeter force term from E = 0.5*gamma*P^2 + lambda*P (maybe -?)
+
+    # Set parameters for each cell type
+    forces.set_params('area', 'passive', {'kappa' : kappa})
+    forces.set_params('perimeter', 'passive',  {'gamma': gamma, 'lambda': lam})
+
     
-    # create_honeycomb_json(
-    #     honeycomb_pth,
-    #     honeycomb_a_len=side_len_rest,
-    # )
+    # #################################################################
+    #
+    # Set conditions for the T1 transition
+    #
+    # #################################################################
+
+    topology.set_params({'min_edge_len': 0.05, 'new_edge_len': 0.055}) 
+
+
+    # #################################################################
+    #
+    # Add Brownian integrator that will handle mechanical part
+    #
+    # #################################################################
+
+    integrators.add('brownian')    
+
+    # #################################################################
+    #
+    # Simulation starts here
+    #
+    # #################################################################
+
+    dt = 0.1
+    integrators.set_dt(0.1) # set time step
+
+    step_size = 10       # Step counter in terms of time units
+
+    # Pulling on the passive system
+    # for i in range(args.nrun):
+    for i in range(10):
+        simulation.run(step_size)
+        dumps.dump_jon("example/res{}.json".format(str(i).zfill(3)))
+        
